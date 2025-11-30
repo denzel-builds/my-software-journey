93f50531feee3b2862f0592c816b29532da8c4f8
denzel-builds <muleyadenzell@gmail.com>
2025-11-30 12:18:19 +0200

    Corrected the withdrawing function to >= rather than just > because what if someone wants to empty their bank account but the code will say insufficient funds

--- day3_bank_account.py
+++ day3_bank_account.py
@@ -9,25 +9,25 @@ class BankAccount:
             print(f"Deposited R{amount:.2f}")
         else:
             print("The amount must be greater than zero")
-        pass
+        
 
     def withdraw (self, amount):
-        if self.balance > amount:
+        if self.balance >= amount:
             self.balance -= amount
             print(f"Successfully withdrawn R {amount:.2f} ")
         else:
             print("Insufficient funds ")
-        pass
+        
 
     def display_balance(self):
         print("Current balance: ", self.balance)
-        pass
+        
 
 my_account = BankAccount("Denzel")
 my_account.deposit(1000)
 my_account.withdraw(500)
-my_account.deposit(75)
-my_account.withdraw(6000)
+my_account.deposit(1)
+my_account.withdraw(501)
 my_account.display_balance()
 
 
